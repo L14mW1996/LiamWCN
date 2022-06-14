981d13b1f798a6434d7a74655cc53a713916ceb4
Liam Walsh <liamjwalsh26@gmail.com>
2022-06-14 13:38:07 +0100

    commit

--- python/Week2/pong.py
+++ python/Week2/pong.py
@@ -4,6 +4,8 @@ pygame.init() #initiates pygame modules and is required in all pygame projects
 
 clock = pygame.time.Clock()
 
+#Code for making the screen
+
 screen_width = 1280
 screen_height = 560
 
@@ -11,11 +13,63 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 
 pygame.display.set_caption("Pong")
 
+def ball_animation():
+    global ball_speed_x, ball_speed_y
+
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
+    if ball.top <= 0 or ball.bottom >= screen_height:
+        ball_speed_y *= -1
+    if ball.left <= 0 or ball.right >= screen_width: 
+        ball_speed_x *= -1
+    if ball.colliderect(player) or ball.colliderect(opponent):
+        ball_speed_x *= -1
+
+
+
+#creating rectangles for game objects
+
+ball = pygame.Rect(screen_width/2 - 15, screen_height/2-15, 30,30)
+
+player = pygame.Rect(screen_width - 20, screen_height/2 - 70, 10, 140)
+
+opponent = pygame.Rect(10, screen_height/2 - 70, 10, 140)
+
+#setting background colours
+
+bg_color = pygame.Color('grey12')
+light_grey = (200,200,200)
+
+#variables for speed
+
+ball_speed_x = 7
+ball_speed_y = 7
+
+
+
+
+
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+    ball_animation()
+    
+    screen.fill(bg_color)
+
+    pygame.draw.rect(screen, light_grey, player)
+    pygame.draw.rect(screen, light_grey, opponent)
+    pygame.draw.ellipse(screen, light_grey,ball)
+
+    pygame.draw.aaline(screen, light_grey,(screen_width/2,0),(screen_width/2,screen_height))
+
+    pygame.display.flip()
+    clock.tick(60)
+
+
+
 
 
 
